3daa96af585699c5ff5a000dbbcc11c65e98af55
Nicholas Day <nick@nickendo.com>
2015-09-19 01:18:52 -0400

    Added support for flask-admin

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from flask_marshmallow import Marshmallow
 import datetime
 import random
 import calendar
+from flask_admin import Admin, expose, AdminIndexView
+from flask_admin.contrib.sqla import ModelView
 ma = Marshmallow()
 
 login_manager = LoginManager()
@@ -19,6 +21,32 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../test.db'
 login_manager.init_app(app)
 ma.init_app(app)
 app.secret_key = 'the power of habit'
+
+class MyModelView(ModelView):
+
+    def is_accessible(self):
+        if current_user.is_authenticated() and (current_user.username == 'nick'):
+            return True
+        else:
+            return False
+
+class MyAdminIndexView(AdminIndexView):
+
+    @expose('/')
+    def index(self):
+        if not current_user.is_authenticated():
+            return redirect(url_for('frontend.login'))
+        if current_user.username == "nick":
+            return super(MyAdminIndexView, self).index()
+        else:
+            return redirect(url_for('frontend.main'))
+
+admin = Admin(app, name='Admin', index_view=MyAdminIndexView(), template_mode='bootstrap3')
+from app.models import User, Note, Task, Due_date
+admin.add_view(MyModelView(User, db.session))
+admin.add_view(MyModelView(Note, db.session))
+admin.add_view(MyModelView(Task, db.session))
+admin.add_view(MyModelView(Due_date, db.session))
 from app.backend import backend
 from app.frontend import frontend
 app.register_blueprint(backend)
