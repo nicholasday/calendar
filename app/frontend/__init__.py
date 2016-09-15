765ef044c81893e4a04cb19d28d953e26724f7ac
Nicholas Day <nick@nickendo.com>
2016-09-15 13:02:35 -0400

    Rewrite first commit

--- app/frontend/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-frontend = Blueprint('frontend', __name__, template_folder='templates')
-
-import app.frontend.calendar
-import app.frontend.users
-import app.frontend.notes
--- run.py
+++
@@ -1,7 +0,0 @@
-#from app import create_app
-from app import app
-
-#app = create_app()
-
-if __name__ == '__main__':
-    app.run(debug=True)
